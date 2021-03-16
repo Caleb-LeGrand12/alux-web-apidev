38c6ce916187900efec59d04518ba1e9a81e9f29
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-03-16 23:05:31 +0200

    refinement

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ import router from "./routes/index.js";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(router);
 
-
 app.listen(port, () => {
-    console.log(`Our Book Club API is now available on port ${port}`)
-})
+  console.log(`Our Book Club API is now available on port ${port}`);
+});
